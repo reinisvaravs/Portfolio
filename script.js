ff1c1138aa50b9862ca89402aa35dd6826f2517d
reinisvaravs <reinisroberts.varavs2@gmail.com>
2024-09-12 03:01:33 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -895,3 +895,50 @@ function validateAll() {
     }
 }
 
+
+document.getElementById("form").addEventListener("submit", function (e) {
+    e.preventDefault(); // Prevent the default form submission
+    document.getElementById("submit").disabled = true;
+
+    // Collect the form data
+    var formData = new FormData(this);
+    var keyValuePairs = [];
+    for (var pair of formData.entries()) {
+      keyValuePairs.push(pair[0] + "=" + pair[1]);
+    }
+
+    var formDataString = keyValuePairs.join("&");
+
+    // Send a POST request to your Google Apps Script
+    fetch(
+      "https://script.google.com/macros/s/AKfycbxHsy6E9Satgt1_iaNIEa-oLIhMblk43LyhLLXDAQOq87qt5FtAmNvgfNd9ViOJ7YEMiA/exec",
+      {
+        redirect: "follow",
+        method: "POST",
+        body: formDataString,
+        headers: {
+          "Content-Type": "text/plain;charset=utf-8",
+        },
+      }
+    )
+      .then(function (response) {
+        // Check if the request was successful
+        if (response) {
+          return response; // Assuming your script returns JSON response
+        } else {
+          throw new Error("Failed to submit the form.");
+        }
+      })
+      .then(function (data) {
+        // Display a success message
+        document.getElementById("submit").disabled = false;
+        document.getElementById("form").reset();
+
+        setTimeout(function () {
+        }, 2600);
+      })
+      .catch(function (error) {
+        // Handle errors, you can display an error message here
+        console.error(error);
+      });
+  });
