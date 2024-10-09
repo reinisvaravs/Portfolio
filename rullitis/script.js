bb4d06a7852d9dea56325d8ddea0fc51cb7bb491
reinisvaravs <reinisroberts.varavs2@gmail.com>
2024-10-09 15:50:34 +0300

    Add files via upload

--- rullitis/script.js
+++ rullitis/script.js
@@ -1,9 +1,14 @@
 function showPage(page) {
     document.querySelectorAll(".pages").forEach(div => {
+        div.classList.remove('active');
         div.style.display = "none";
     });
 
-    document.querySelector(`#${page}`).style.display = "block";
+    const targetPage = document.querySelector(`#${page}`);
+    targetPage.style.display = "block";
+    requestAnimationFrame(() => {
+        targetPage.classList.add('active');
+    });
 
     document.querySelectorAll(".page_buttons").forEach(button => {
         button.classList.remove("active");
@@ -12,17 +17,16 @@ function showPage(page) {
     document.querySelectorAll(`button[data-page="${page}"]`).forEach(button => {
         button.classList.add("active");
     });
-};
+}
 
 document.querySelectorAll(".page_buttons").forEach(button => {
     button.onclick = function() {
-        setTimeout(() => {
-            showPage(this.dataset.page);
-        }, 100);
+        showPage(this.dataset.page);
     }
 });
 
 window.onload = () => {
+    document.querySelector("#page1").classList.add('active');
     document.querySelectorAll(`button[data-page="page1"]`).forEach(button => {
         button.classList.add("active");
     });
