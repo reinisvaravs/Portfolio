aac204228c117484d6eb98f66ddebbd7a240c9d1
reinisvaravs <reinisroberts.varavs2@gmail.com>
2024-10-09 15:45:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+function showPage(page) {
+    document.querySelectorAll(".pages").forEach(div => {
+        div.classList.remove('active');
+        div.style.display = "none";
+    });
+
+    const targetPage = document.querySelector(`#${page}`);
+    targetPage.style.display = "block";
+    requestAnimationFrame(() => {
+        targetPage.classList.add('active');
+    });
+
+    document.querySelectorAll(".page_buttons").forEach(button => {
+        button.classList.remove("active");
+    });
+
+    document.querySelectorAll(`button[data-page="${page}"]`).forEach(button => {
+        button.classList.add("active");
+    });
+}
+
+document.querySelectorAll(".page_buttons").forEach(button => {
+    button.onclick = function() {
+        showPage(this.dataset.page);
+    }
+});
+
+window.onload = () => {
+    document.querySelector("#page1").classList.add('active');
+    document.querySelectorAll(`button[data-page="page1"]`).forEach(button => {
+        button.classList.add("active");
+    });
+}
