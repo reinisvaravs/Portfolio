1d61c51dea5bd5de7fda0ac8ad9c6e51c488f94f
reinisvaravs <reinisroberts.varavs2@gmail.com>
2024-10-09 19:49:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const projectsBtn = document.getElementById("projects-btn")
+const work = document.getElementById("work");
+
+projectsBtn.onclick = () => {
+    window.scroll({
+        top: 2500, 
+        left: 0, 
+        behavior: 'smooth' 
+    });
+
+}
