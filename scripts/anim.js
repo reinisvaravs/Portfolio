d85fdfe0b395e982863d22883de0bb595e0e617d
reinisvaravs <reinisroberts.varavs2@gmail.com>
2024-10-22 16:18:33 +0300

    Add files via upload

--- scripts/anim.js
+++ scripts/anim.js
@@ -1,12 +1,26 @@
+
+const display1 = document.getElementById("display-1")
+const display2 = document.getElementById("display-2")
+
+
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
             entry.target.classList.add("show")
-        } else {
+            if (entry.target.id === "display-1" && entry.isIntersecting) {
+                display1.classList.add("fadeIn");
+            }
+            if (entry.target.id === "display-2" && entry.isIntersecting) {
+                display2.classList.add("fadeIn");
+            }
+        }
+        else {
             entry.target.classList.remove("show")
+            display1.classList.remove("fadeIn");
+            display2.classList.remove("fadeIn");
         }
     })
 })
 
-const hiddenElements = document.querySelectorAll(".hidden")
+const hiddenElements = document.querySelectorAll(".hidden, #display-1, #display-2")
 hiddenElements.forEach((el) => observer.observe(el))
