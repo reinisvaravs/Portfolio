fab68479185fd361f46569d39a3a0ae149df9d1f
reinisvaravs <reinisroberts.varavs2@gmail.com>
2024-10-24 19:59:20 +0300

    Add files via upload

--- restaurant/script.js
+++ restaurant/script.js
@@ -10,3 +10,29 @@ document.querySelector("body").onscroll = function() {
     // Apply the y-position as a CSS variable to the hero section
     document.getElementById("hero").style.setProperty('--bg-position', yvalue + "px");
 }
+
+
+const heroHeight = document.getElementById("hero").offsetHeight
+
+document.getElementById("hero-arrow").onclick = () => {
+    window.scroll({
+        top: heroHeight, 
+        left: 0, 
+        behavior: 'smooth' 
+    });
+}
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show")
+        }
+        else {
+            entry.target.classList.remove("show")
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll(".hidden, .hidden1, .hidden2, .hidden3")
+hiddenElements.forEach((el) => observer.observe(el))
