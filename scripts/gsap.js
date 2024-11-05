bc09741ffdd304255dd2b813b6ee7880e4040c4e
reinisvaravs <reinisroberts.varavs2@gmail.com>
2024-11-05 18:26:31 +0200

    Add files via upload

--- scripts/gsap.js
+++ scripts/gsap.js
@@ -1,7 +1,7 @@
 gsap.registerPlugin(ScrollTrigger)
 const lenis = new Lenis();
 lenis.on("scroll", ScrollTrigger.update);
-gsap.ticker.add((time) => { lenis.raf(time * 4000); });
+gsap.ticker.add((time) => { lenis.raf(time * 1000); });
 gsap.ticker.lagSmoothing(0);
 
 
